eba1b657d5db297d1baa1b8ce6f6f9bbc2eabaeb
namanQuick <namanb487@gmail.com>
2021-03-09 09:46:09 +0530

    Coin Change

---
+++ coinChange/CoinChange.java
@@ -0,0 +1,36 @@
+package coinChange;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class CoinChange{
+    public static int coin(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                if(coins[j] == i) {
+                    dp[i] = 1;
+                }
+                else if(coins[j] < i && dp[i - coins[j]] > 0) {
+                    dp[i] = (dp[i] == -1) ? dp[i - coins[j]] + 1 : Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        
+        return dp[amount];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i=0; i<n; i++){
+            coins[i] = sc.nextInt();
+        }
+        int amount = sc.nextInt();
+        int ans = coin(coins, amount);
+        System.out.println(ans);
+    }
+}
