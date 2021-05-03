d17281aa9a00b2b28c796a3cd0472bcaf02acfcb
namanQuick <namanb487@gmail.com>
2021-05-03 14:10:02 +0530

    Animal Shelter

---
+++ CtCi/Stacks_and_Queues/AnimalShelter/Cat.java
@@ -0,0 +1,12 @@
+package CtCi.Stacks_and_Queues.AnimalShelter;
+
+public class Cat extends Animal {
+    public Cat(String n) {
+        super(n);
+    }
+        
+    public String name() {
+        return "Cat: " + name;
+    }
+}
+
---
+++ CtCi/Stacks_and_Queues/AnimalShelter/Dog.java
@@ -0,0 +1,11 @@
+package CtCi.Stacks_and_Queues.AnimalShelter;
+
+public class Dog extends Animal{
+    public Dog(String n) {
+		super(n);
+	}
+	
+	public String name() {
+		return "Dog: " + name;
+	}
+}
