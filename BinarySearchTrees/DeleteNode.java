8652e2bce29ded56dd19f32b9806ada0b09e4d6a
namanQuick <namanb487@gmail.com>
2021-08-06 18:10:16 +0530

    delete BST node bug fix

--- BinarySearchTrees/DeleteNode.java
+++ BinarySearchTrees/DeleteNode.java
@@ -4,33 +4,24 @@ import CtCi.Trees_and_Graphs.TreeNode;
 
 public class DeleteNode {
     public static TreeNode deleteNode(TreeNode root, int key){
-        
-        if(root==null){
-            return null;
-        }
-        
-        if(root.data<key){
-            root.right=deleteNode(root.right,key);
-        }else if(root.data>key){
-            root.left=deleteNode(root.left,key);
-        }else{
-            if(root.left==null || root.right==null){
-                return root.left!=null ? root.left:root.right;
+        if(root == null) return null;
+        if(root.data < key) {
+            root.right = deleteNode(root.right, key);
+        } else if(root.data > key){
+            root.left = deleteNode(root.left, key);
+        } else {
+            if(root.left != null && root.right !=null){
+                TreeNode current  = root.right;
+                TreeNode temp = current;
+                while(temp.left != null){
+                    temp = temp.left;
+                }
+                temp.left = root.left;
+                return current;
             }
-            
-            int maxData=maximum(root.left);
-            root.data=maxData;
-            root.left=deleteNode(root.left,maxData);
+            return root.left == null ? root.right : root.left;
         }
-        
         return root;
-    }
-
-    private static int maximum(TreeNode root){
-        while(root.right!=null){
-            root=root.right;
-        }
-        return root.data;
     }
 
     public static void main(String[] args) {
