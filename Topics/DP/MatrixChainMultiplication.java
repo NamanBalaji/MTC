b4cb54f119ece2e594d166fd7bc00ad44b904cd4
namanQuick <namanb487@gmail.com>
2021-09-25 14:11:55 +0530

    matrix chain multiplication(rec)

--- Topics/DP/MatrixChainMultiplication.java
+++ Topics/DP/MatrixChainMultiplication.java
@@ -19,9 +19,21 @@ public class MatrixChainMultiplication {
         return memo[0][arr.length-1];
     }
 
+    public static int matrixChainRecursive(int i, int j, int arr[]){
+        if(i==j)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        for(int k = i; k<j; k++){
+            int cost = matrixChainRecursive(i, k, arr) + matrixChainRecursive(k+1, j, arr) + arr[i-1]*arr[k]*arr[j];
+            min = Math.min(cost, min);
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         int arr[] = {4,2,3,5,3};
         int cost = findCost(arr);
         System.out.println(cost);
+        System.out.println(matrixChainRecursive(1, 4, arr));
     }
 }
